7e4769894b70b146964a297b9890e4e4d8877e96
Pradeep Singh <99412773+Pradeep330Singh@users.noreply.github.com>
2024-07-04 14:19:20 +0545

    Add files via upload

---
+++ typeconversion.py
@@ -0,0 +1,23 @@
+# Prompting the user for a number
+user_input = input("Enter a number: ")
+
+# Converting the input into an integer
+integer_number = int(user_input)
+
+# Printing the converted integer
+print("Converted integer:", integer_number)
+
+# Converting the integer into a float
+float_number = float(integer_number)
+
+# Printing the converted float
+print("Converted float:", float_number)
+
+# Converting the float into a string
+string_number = str(float_number)
+
+# Printing all the converted strings
+print("Converted strings:")
+print("Integer as string:", str(integer_number))
+print("Float as string:", str(float_number))
+print("Converted float as string:", string_number)
